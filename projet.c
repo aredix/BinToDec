e88ccefa346fde481c7f163a48f893b8bd207f32
aredix <32569871+aredix@users.noreply.github.com>
2018-10-15 22:12:36 +0200

    Update projet.c

--- projet.c
+++ projet.c
@@ -8,15 +8,15 @@ int main()
     int longueur;
     int valeur;
     int k=0;
+    int resultat;
 
     printf("Longueur du nombre binaire :\n");
     scanf("%d", &longueur);
     int tabBinaire[longueur];
     int i = longueur;
     int j = longueur;
-    //system("cls");
+    system("cls");
 
-    printf("longueur : %d\n", longueur);
     printf("Valeur binaire :\n");
     scanf("%d", &valeur);
 
@@ -30,23 +30,29 @@ int main()
         i--;
     }
 
-    printf(" val tab [3] : %d\n", tabBinaire[3]);
-    printf(" val tab [2] : %d\n", tabBinaire[2]);
-    printf(" val tab [1] : %d\n", tabBinaire[1]);
-    printf(" val tab [0] : %d\n", tabBinaire[0]);
     /* Calcul de la valeur décimal */
 
     while (j>0)
     {
-        printf("val de k : %d\n", k);
-        printf("val de tab de k : %d\n", tabBinaire[k]);
-        temp = temp + pow(2, tabBinaire[k]);
-        k=k+1;
+
+        if (tabBinaire[k]==1 || tabBinaire[k] ==0)
+        {
+            if (tabBinaire[k]==1)
+            {
+                temp = temp + pow(2, k);
+            }
+            k++;
+        }
+        else
+        {
+            printf("Votre nombre n'est pas au format binaire ! ");
+        }
         j--;
     }
 
-    //system("cls");
-    printf("Resulat : %f\n", temp);
+    system("cls");
+    resultat = temp;
+    printf("Resulat : %d\n", resultat);
 
 return 0;
 }
