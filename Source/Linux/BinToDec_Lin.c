0e061308212339b84076fc9a9aef88a855708ae7
test <test@213-99.iutbeziers.fr>
2018-10-16 11:31:33 +0200

    Ajouts de commentaire + Description

--- Source/Linux/BinToDec_Lin.c
+++ Source/Linux/BinToDec_Lin.c
@@ -1,3 +1,6 @@
+// Author : IsatiX / Aredix
+// Date : 16/10/2018 (FR Format)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -38,6 +41,8 @@ int main()
     while (j>0)
     {
 
+        /* Vérification de la valeur dans le tableau */
+
         if (tabBinaire[j-1]==1 || tabBinaire[j-1] ==0)
         {
             if (tabBinaire[j-1]==1)
@@ -46,6 +51,9 @@ int main()
             }
             k++;
         }
+
+        /* Si la valeur est différente de 1 ou de 0 on sort du programme */
+
         else
         {
             printf("Votre nombre n'est pas au format binaire ! \n");
@@ -53,8 +61,8 @@ int main()
         }
         j--;
         system("clear");
-        resultat = temp;
-        printf("Resulat : %d\n", resultat);
+        resultat = temp; // converstion de la valeur de float à int
+        printf("Resulat : %d\n", resultat); // Affichage du résultat de la conversion
     }
 
 return 0;
